5572bacc914fde8d7987c2f70bcf4e2dcf4e9324
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-09-10 02:05:38 +0700

    creating lib dir

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT | 3000;
 const path = require('path')
 
+const getFortune = require('./lib/fortune_generator.js')
+
 const hbsEngine = expressHandlebars.create({
     defaultLayout: 'main',
     layoutsDir: path.join(__dirname, 'views/layouts'),
@@ -14,7 +16,9 @@ app.engine('handlebars', hbsEngine.engine)
 app.set('view engine', 'handlebars')
 
 app.get("/", (req, res) => {
-    res.render('home')
+    res.render('home', {
+        quote: getFortune.getFortune()
+    })
 })
 
 app.get("/about",(req, res) => {

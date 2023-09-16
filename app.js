46508cd958aae90fb79f2d03ad91a737b748b2fb
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-09-16 20:44:42 +0700

    sebelum ch8 dari buku

--- app.js
+++ app.js
@@ -4,19 +4,36 @@ const app = express();
 const port = process.env.PORT | 3000;
 const path = require('path')
 const h = require('./lib/handlers.js')
+const weatherMid = require('./lib/middlewares/weather.js')
 
 const hbsEngine = expressHandlebars.create({
     defaultLayout: 'main',
     layoutsDir: path.join(__dirname, 'views/layouts'),
+    // 1.4 deklarasi path untuk partials secara manual
+    partialsDir: path.join(__dirname, 'views/partials'),
+    helpers: {
+        section: 
+            function (name, options){
+                if(!this._sections) this._sections = {} 
+                this._sections[name] = options.fn(this) 
+                return null
+            }
+    }
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.engine('handlebars', hbsEngine.engine)
 app.set('view engine', 'handlebars')
+
+// 1.5 mengubah urutan middleware dibawah view engine
+app.use(weatherMid.weatherMiddleware)
 
 app.get("/", h.home)
 
 app.get("/about", h.about)
+
+app.get("/head",h.header)
 
 app.use(h.notFound)
 
@@ -24,7 +41,7 @@ app.use(h.intError)
 
 if(require.main == module){
     app.listen(port, () => {
-        console.log(`im listening on port ${port}`)
+        console.log(`HTTP Listening on http://localhost:${port}`)
     })
 }else{
     module.exports = app
